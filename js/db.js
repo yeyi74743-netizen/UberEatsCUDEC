762fecb581c78f4574b0f9d723659ef5890f9e47
yeyi74743-netizen <yeyi74743@gmail.com>
2026-06-04 21:25:09 -0600

    se mostraron los platillos en la app

--- js/db.js
+++ js/db.js
@@ -1,5 +1,5 @@
 db.collection("platillos").onSnapshot((datos) => {
     datos.forEach((registro) => {
-        console.log(registro);
+       mostrarPlatillo(registro.data(),registro.id);
     });
 });
--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+let contenido = '';
 btnAgregarPlatillo = document.getElementById('btnAgregarPlatillo');
 document.addEventListener('DOMContentLoaded', function() {
   // nav menu
@@ -11,3 +12,23 @@ document.addEventListener('DOMContentLoaded', function() {
 btnAgregarPlatillo.addEventListener('click',function() {
 alert('Platillo agregado');
 });
+function mostrarPlatillo(platillo, id) {
+  contenido +=`
+  <div class="card-panel recipe white row" data-id="${id}">
+   <div class="recipe-details">
+    <div class="recipe-title">
+          ${platillo.nombre}
+        </div>
+        <div class="recipe-ingredients">
+  ${platillo.ingredientes}
+    <div class="recipe-prize">
+          precio$ ${platillo.precio}
+    </div>
+   </div>
+  </div>
+  <div class="recipe-delete">
+    <i class="material-icons" data-id="${id}">delete_outline</i>
+  </div>
+ </div>`; 
+  document.querySelector('.recipes').innerHTML = contenido;
+}
