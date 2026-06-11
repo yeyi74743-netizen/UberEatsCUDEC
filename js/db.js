4288e3c3fe0110eea92b66b9e0d1da37be423bba
yeyi74743-netizen <yeyi74743@gmail.com>
2026-06-10 21:28:42 -0600

    Configuracion de boton para agregar platillos

--- js/db.js
+++ js/db.js
@@ -1,5 +1,30 @@
 db.collection("platillos").onSnapshot((datos) => {
-    datos.forEach((registro) => {
-       mostrarPlatillo(registro.data(),registro.id);
+    datos.docChanges().forEach((registro) => {
+        if (registro.type === "added") {
+            mostrarPlatillo(registro.doc.data(),registro.doc.id);
+        }
+       if (registro.type === "modified") {
+        actualizarPlatillo(registro.doc.data(),registro.doc.id);
+       }
     });
+});
+
+const formularioAgregar = document.querySelector("form");
+formularioAgregar.addEventListener("submit",(e) => {
+    e.preventDefault();
+    const platilloNuevo = {
+        nombre: formularioAgregar.title.value,
+        ingredientes: formularioAgregar.ingredients.value,
+        precio: formularioAgregar.price.value
+    };
+    db.collection("platillos").add(platilloNuevo)
+    .catch((error) => {
+        console.log(error); 
+        alert("Error al agregar platillo");
+    }
+    );
+    formularioAgregar.title.value = "";
+    formularioAgregar.ingredients.value = "";
+    formularioAgregar.precio.value = "";
+    alert("Platillo agregado");
 });
--- js/index.js
+++ js/index.js
@@ -9,19 +9,17 @@ document.addEventListener('DOMContentLoaded', function() {
   M.Sidenav.init(forms, {edge: 'left'});
 });
 
-btnAgregarPlatillo.addEventListener('click',function() {
-alert('Platillo agregado');
-});
+
 function mostrarPlatillo(platillo, id) {
   contenido +=`
-  <div class="card-panel recipe white row" data-id="${id}">
+  <div class="card-panel recipe white row" id="${id}">
    <div class="recipe-details">
     <div class="recipe-title">
           ${platillo.nombre}
         </div>
         <div class="recipe-ingredients">
   ${platillo.ingredientes}
-    <div class="recipe-prize">
+    <div class="recipe-price">
           precio$ ${platillo.precio}
     </div>
    </div>
@@ -31,4 +29,10 @@ function mostrarPlatillo(platillo, id) {
   </div>
  </div>`; 
   document.querySelector('.recipes').innerHTML = contenido;
+}
+function actualizarPlatillo(platillo,id) {
+  let tarjeta = document.getElementById (`${id}`);
+  tarjeta.querySelector(".recipe-title").innerHTML = platillo.nombre;
+  tarjeta.querySelector(".recipe-ingredients").innerHTML = platillo.ingredientes;
+  tarjeta.querySelector(".recipe-price").innerHTML = platillo.precio;
 }
