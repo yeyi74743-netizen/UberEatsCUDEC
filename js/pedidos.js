98ad6a0cafc7602d35ba21d37d483b9a8612fb9f
yeyi74743-netizen <yeyi74743@gmail.com>
2026-07-09 21:16:32 -0600

    se agrego el mapa de la ubicacion y el boton de imagen

--- js/pedidos.js
+++ js/pedidos.js
@@ -93,7 +93,13 @@ function exito(posicion) {
 
             
             M.updateTextFields();
+            var map = L.map('mapa').setView([latitud, longitud], 13);
 
+            L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+                maxZoom: 19,
+                attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            }).addTo(map);
+            var marker = L.marker([latitud, longitud]).addTo(map);
         })
         .catch(err => {
 
